68b62205fe36d82b0f132bc7e25a782895bfac92
Sasquire <32607148+Sasquire@users.noreply.github.com>
2018-12-27 18:43:32 -0500

    giving up on script injection. maybe later

--- source/application_min_changes/initialize_changes.js
+++ source/application_min_changes/initialize_changes.js
@@ -2,17 +2,17 @@ const IC = {}; // namespace
 
 IC.files = [
 	'better_blacklist.js'
-].map(e => chrome.extension.getURL('./source/application_min_changes/'+e))
+].map(e => chrome.extension.getURL('./source/application_min_changes/'+e));
 
 IC.inject_page_script = function(mutationsList, observer) {
-	const maybe_app_min =	mutationsList
+	const maybe_app_min = mutationsList
 		.filter(mutation => 
 			mutation.addedNodes[0] &&
 			mutation.addedNodes[0].tagName === 'SCRIPT' &&
-			mutation.addedNodes[0].src.match(/application-min/))
+			mutation.addedNodes[0].src.match(/application-min/));
 	if(maybe_app_min.length == 0){ return; }
-	IC.observer.disconnect();
 	const app_min = maybe_app_min[0].addedNodes[0];
+	observer.disconnect();
 	IC.files.forEach(url => {
 		const node = document.createElement('script');
 		node.src = url;
@@ -20,5 +20,4 @@ IC.inject_page_script = function(mutationsList, observer) {
 	});	
 };
 
-IC.observer = new MutationObserver(IC.inject_page_script);
-IC.observer.observe(document, { childList: true, subtree: true });
+new MutationObserver(IC.inject_page_script).observe(document, {childList:true,subtree:true});
