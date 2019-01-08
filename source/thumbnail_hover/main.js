24dfe52cb23acda0735b71d6056189f20dd9b310
Sasquire <32607148+Sasquire@users.noreply.github.com>
2019-01-08 16:47:04 -0500

    improvements to thumbnail hover

--- source/thumbnail_hover/main.js
+++ source/thumbnail_hover/main.js
@@ -1,36 +1,48 @@
+// todo make this whole thing cleaner
 (function(){
-	/*
-	const links = Array.from(document.getElementsByClassName('thumb'))
-		.map(n => n.querySelector('a'))
-	const images = links.map(n => {
-		const md5 = n.querySelector('img').src.match(/[0-9abcdef]{32}|download-preview\.png|deleted-preview.png/)[0]
-		if(md5.length != 32){ return `https://static.e621.net/images${md5}`}
-		return md5;
-	})
-	console.log(images) */
-
-	const script = document.createElement('script');
-	script.textContent = `
-		const thumbs = Array.from(document.getElementsByClassName('thumb'));
-		const posts = thumbs.map(e => parseInt(e.id.substring(1)))
-			.forEach(n => {
-				document.body.innerHTML += '<img id="ith_'+n+'" src="'+Post.posts.get(n).file_url+'" class="ith_img">'
-			})
-	`
-	document.head.appendChild(script);
-	script.remove();
+	document.body.innerHTML += '<div id="idt_image_bank"></div>';
+	add_script(`
+		Array.from(document.querySelectorAll('.thumb img'))
+			.forEach(n => n.addEventListener('mousemove', (e) => {
+				const id = e.path[2].id.substring(1);
+				const img = document.getElementById('ith_'+id);			
+				if(img){ return; }
+				document.getElementById('idt_image_bank').innerHTML += '<img id="ith_'+id+'" src="'+Post.posts.get(id).file_url+'" class="ith_img">';
+			}));
+	`);
 
 	Array.from(document.querySelectorAll('.thumb img'))
-		.forEach(n => n.addEventListener('mousemove', (e) => {
-			//Array.from(document.getElementsByClassName('ith_img')).forEach(n => n.style.display = 'none');
-			const img = document.getElementById('ith_'+(e.path[2].id.substring(1)));
-			img.style.display = 'block';
-			img.style.left = e.clientX+'px';
-			img.style.top = e.clientY+'px';
-		}));
+		.forEach(n => {
+			n.title = '';
+			n.addEventListener('mousemove', display_image);
+			n.addEventListener('mouseout', hide_image);
+		});
 
-		Array.from(document.querySelectorAll('.thumb img'))
-		.forEach(n => n.addEventListener('mouseout', (e) => 
-			document.getElementById('ith_'+(e.path[2].id.substring(1))).style.display = 'none'
-		));
+	function display_image(e){
+		// todo broken when scrolling
+		// todo webm's do not play/ give invalid image errors
+		const img = document.getElementById('ith_'+(e.path[2].id.substring(1)));
+		img.style.display = 'block';
+		
+		// todo problems when looking at posts on the bottom of the page
+		img.style.top = e.clientY+10+'px';
+		
+		// todo window.innerWidth doesnt count scrollbars
+		if(e.clientX+10+img.offsetWidth > window.innerWidth){
+			img.style.left = e.clientX - (img.width+10)+'px';
+		} else {
+			img.style.left = e.clientX+10+'px';
+		}
+	}
+	
+	function hide_image(e){
+		document.getElementById('ith_'+(e.path[2].id.substring(1))).style.display = 'none';
+	}
+
+	function add_script(text){
+		const script = document.createElement('script');
+		script.textContent = text;
+		document.head.appendChild(script);
+		script.remove();
+	}
 })();
