87c51d140712afef93f27a742bc489c98917d5fa
Sasquire <32607148+Sasquire@users.noreply.github.com>
2019-01-08 02:45:21 -0500

    implemented quick set add

---
+++ source/quick_set_add/main.js
@@ -0,0 +1,30 @@
+(async function(){
+	const page_id = parseInt(window.location.href.match(/\/(\d+).*/)[1]);
+	const sets = await download_sets();
+
+	document.getElementById('subnav').innerHTML += `
+		<ul id="iqsa_ul" class="flat-list">
+			${sets.map(e => `<li><button value="${e.id}" class="iqsa_btn">${e.name}</button></li>`).join('')}
+		</ul>`;
+
+	const script = document.createElement('script');
+	script.textContent = `Array.from(document.getElementsByClassName('iqsa_btn'))
+		.forEach(n => n.onclick = function(){
+			PostSet.add_post(${page_id}, this.value);
+		})`;
+	document.head.appendChild(script);
+	script.remove();
+
+	async function download_sets(){
+		const url_obj = new URL('https://e621.net/set/select')
+		url_obj.searchParams.set('id', page_id);
+		const fetch_req = new Request(url_obj.href)
+		const page_text = await fetch(fetch_req).then(res => res.text());
+		const doc = new DOMParser().parseFromString(page_text, "text/html");
+		return Array.from(doc.getElementsByTagName('option'))
+			.map(n => ({
+				id: parseInt(n.value),
+				name:n.innerText
+			}));
+	};
+}());
